4cdff8d43c70edefbabd8eccfadbe05180a43212
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-04-11 18:49:40 +0200

    end of call

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,32 @@ function union2(arr1, arr2) {
   return result;
 }
 
+const arr1 = [1, 2, 3, 3, 4];
+const arr2 = [1, 2, 4, 5, 1, 11, 7, 8];
+
+const setA = arrayToSet(arr1);
+const setB = arrayToSet(arr2);
+const united = union2(setA, setB);
+
+function Card({ array, title }) {
+  return (
+    <section>
+      <header>
+        <h1>{title}</h1>
+      </header>
+      {array.map((item) => {
+        return <span key={item}>{item}&nbsp;</span>;
+      })}
+    </section>
+  );
+}
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Card array={setA} title={"Set A"} />
+      <Card array={setB} title={"Set B"} />
+      <Card array={united} title={"Union"} />
+    </>
+  );
 }
